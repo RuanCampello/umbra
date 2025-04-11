d25e17359ed4a27c6486ad038510e588f6fa4cba
RuanCampello <ruancampello888@gmail.com>
2025-04-11 20:05:07 -0300

    feat: add display and string/char implementations

--- src/sql/tokens.rs
+++ src/sql/tokens.rs
@@ -1,14 +1,14 @@
-//! Tokens definitions for SQL.
+//! Token's definitions for SQL.
 //!
 //! You may consider checking out [IBM documentation](https://www.ibm.com/docs/en/db2/11.5.0?topic=elements-tokens) on that.
 
-use std::fmt::Display;
+use std::fmt::{Display, Formatter, Write};
 
 /// SQL tokens.
 pub(in crate::sql) enum Token {
     Whitespace(Whitespace),
     Identifier(String),
-    Keyword(String),
+    Keyword(Keyword),
     Number(String),
     Eq,
     Neq,
@@ -35,6 +35,7 @@ pub(in crate::sql) enum Whitespace {
 /// A little subset of SQL keywords.
 ///
 /// [Here](https://www.ibm.com/docs/en/informix-servers/12.10.0?topic=appendixes-keywords-sql-informix) there are much more than that, if you want to check out.
+#[derive(Clone, Copy)]
 pub(in crate::sql) enum Keyword {
     Select,
     Create,
@@ -70,4 +71,104 @@ pub(in crate::sql) enum Keyword {
     Rollback,
     Commit,
     Explain,
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Whitespace(whitespace) => f.write_char(whitespace.as_char()),
+            Self::Identifier(identifier) => f.write_str(identifier),
+            Self::Keyword(keyword) => f.write_str(keyword.as_str()),
+            Self::Number(number) => f.write_str(number),
+            token => {
+                let s = match token {
+                    Self::Eq => "=",
+                    Self::Neq => "!=",
+                    Self::Gt => ">",
+                    Self::Lt => "<",
+                    Self::GtEq => ">=",
+                    Self::LtEq => "<=",
+                    Self::Mul => "*",
+                    Self::Div => "/",
+                    Self::Plus => "+",
+                    Self::Minus => "-",
+                    Self::Comma => ",",
+                    Self::Semicolon => ";",
+                    Self::LeftParen => "(",
+                    Self::RightParen => ")",
+                    _ => unreachable!(),
+                };
+                f.write_str(s)
+            }
+        }
+    }
+}
+
+impl Whitespace {
+    fn as_char(&self) -> char {
+        match self {
+            Self::Tab => '\t',
+            Self::Space => ' ',
+            Self::Newline => '\n',
+        }
+    }
+}
+
+impl Display for Whitespace {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_char(self.as_char())
+    }
+}
+
+impl Keyword {
+    fn as_str(&self) -> &'static str {
+        match self {
+            Self::Select => "SELECT",
+            Self::Create => "CREATE",
+            Self::Update => "UPDATE",
+            Self::Delete => "DELETE",
+            Self::Insert => "INSERT",
+            Self::Into => "INTO",
+            Self::Values => "VALUES",
+            Self::Set => "SET",
+            Self::Drop => "DROP",
+            Self::Where => "WHERE",
+            Self::From => "FROM",
+            Self::And => "AND",
+            Self::Or => "OR",
+            Self::Primary => "PRIMARY",
+            Self::Key => "KEY",
+            Self::Unique => "UNIQUE",
+            Self::Table => "TABLE",
+            Self::Database => "DATABASE",
+            Self::Int => "INT",
+            Self::BigInt => "BIGINT",
+            Self::Unsigned => "UNSIGNED",
+            Self::Varchar => "VARCHAR",
+            Self::Bool => "BOOL",
+            Self::True => "TRUE",
+            Self::False => "FALSE",
+            Self::Index => "INDEX",
+            Self::Order => "ORDER",
+            Self::By => "BY",
+            Self::On => "ON",
+            Self::Start => "BEGIN",
+            Self::Transaction => "TRANSACTION",
+            Self::Rollback => "ROLLBACK",
+            Self::Commit => "COMMIT",
+            Self::Explain => "EXPLAIN",
+        }
+    }
+}
+
+impl Display for Keyword {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str(self.as_str())
+    }
+}
+
+impl From<&Keyword> for Token {
+    fn from(keyword: &Keyword) -> Self {
+        Token::Keyword(*keyword)
+    }
 }
