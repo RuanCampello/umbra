2edb03f40d5fb911b41aa46acb43adeafc1983d9
RuanCampello <ruancampello888@gmail.com>
2025-04-14 20:25:50 -0300

    feat: implementation of column for table create

--- src/sql/statement.rs
+++ src/sql/statement.rs
@@ -2,6 +2,9 @@
 //! See [this](https://en.wikipedia.org/wiki/Abstract_syntax_tree)
 //! to reach out about statements and/or AST.
 
+use std::cmp::Ordering;
+use std::fmt::{Display, Write};
+
 /// SQL statements,
 pub(crate) enum Statement {
     Create(String),
@@ -34,16 +37,43 @@ pub(crate) struct Assignment {
     pub value: Expression,
 }
 
+pub(crate) struct Column {
+    name: String,
+    data_type: Type,
+    constraints: Vec<Constraint>,
+}
+
+pub(crate) enum Create {
+    Database(String),
+    Table {
+        name: String,
+        columns: Vec<Column>,
+    },
+    Index {
+        name: String,
+        table: String,
+        column: String,
+        unique: bool,
+    },
+}
+
 pub(crate) enum Expression {
     Identifier(String),
     Value(String),
     Nested(Box<Self>),
 }
 
+#[derive(PartialEq)]
 pub(crate) enum Value {
     String(String),
     Number(i128),
     Boolean(bool),
+}
+
+#[derive(Debug, PartialEq)]
+enum Constraint {
+    PrimaryKey,
+    Unique,
 }
 
 /// SQL data types.
@@ -55,4 +85,60 @@ pub(crate) enum Type {
     UnsignedBigInteger,
     Boolean,
     Varchar(usize),
+}
+
+impl Column {
+    pub fn new(name: &str, data_type: Type) -> Self {
+        Self {
+            name: name.to_string(),
+            data_type,
+            constraints: vec![],
+        }
+    }
+
+    pub fn primary_key(name: &str, data_type: Type) -> Self {
+        Self {
+            name: name.to_string(),
+            data_type,
+            constraints: vec![Constraint::PrimaryKey],
+        }
+    }
+
+    pub fn unique(name: &str, data_type: Type) -> Self {
+        Self {
+            name: name.to_string(),
+            data_type,
+            constraints: vec![Constraint::Unique],
+        }
+    }
+}
+
+impl PartialOrd for Value {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match (self, other) {
+            (Value::Number(a), Value::Number(b)) => a.partial_cmp(b),
+            (Value::String(a), Value::String(b)) => a.partial_cmp(b),
+            (Value::Boolean(a), Value::Boolean(b)) => a.partial_cmp(b),
+            _ => None,
+        }
+    }
+}
+
+pub(crate) fn join<'t, T: Display + 't>(
+    values: impl IntoIterator<Item = &'t T>,
+    separator: &str,
+) -> String {
+    let mut joined = String::new();
+    let mut iter = values.into_iter();
+
+    if let Some(value) = iter.next() {
+        write!(joined, "{}", &value).unwrap();
+    }
+
+    for value in iter {
+        joined.push_str(separator);
+        write!(joined, "{value}").unwrap();
+    }
+
+    joined
 }
