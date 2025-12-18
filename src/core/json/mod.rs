47cbe7bb44407e9fe8ad9df0f12fffd7cbfddf4c
RuanCampello <ruancampello999@gmail.com>
2025-12-17 23:59:36 -0300

    feat: add conversion enum

--- src/core/json/mod.rs
+++ src/core/json/mod.rs
@@ -1,9 +1,22 @@
 use error::Error as JsonError;
 
+use crate::sql::statement::Value;
+
 mod cache;
 pub mod error;
 mod jsonb;
 mod ops;
 mod path;
 
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Conv {
+    Strict,
+    NotStrict,
+    ToString,
+}
+
 pub type Result<T> = std::result::Result<T, JsonError>;
+
+pub fn from_value_to_jsonb(value: &Value, strict: Conv) -> crate::Result<jsonb::Jsonb> {
+    todo!()
+}
